0d6db0c3a5fef39466e438c93b8b253b215291f3
hoseony <hoseon.alex.yu@gmail.com>
2026-08-09 16:15:49 -0700

    forgot to push log.c previously

---
+++ mavlink/src/log.c
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "../include/mavlink_types.h"
+
+bool log_create(FILE **file) {
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+
+    char time_str[64];
+    strftime(time_str, sizeof(time_str), "../out/attitude_%Y%m%d_%H_%M_%S.csv", t);
+   
+    *file = fopen(time_str, "w");
+    
+    if (file == NULL) {
+        perror("fopen");
+        return false;
+    }
+
+    return true;
+}
+
+void log_write_attitude(FILE *file, const MAVLinkAttitude_t *attitude) {
+    fprintf(
+        file,
+        "%u,%.9g,%.9g,%.9g,%.9g,%.9g,%.9g\n",
+        attitude->time_boot_ms,
+        attitude->roll,
+        attitude->pitch,
+        attitude->yaw,
+        attitude->rollspeed,
+        attitude->pitchspeed,
+        attitude->yawspeed
+    );
+    
+    fflush(file);
+}
+
+
+int main() {
+    FILE *file = NULL;
+    log_create(&file);
+}
